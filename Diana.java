f5fb12b797145be9a5530e41f7ea8f8f16235adb
Diana Zibi <76133584+dianayonipha@users.noreply.github.com>
2021-05-30 23:07:21 +0200

    Add files via upload

---
+++ Diana.java
@@ -0,0 +1,12 @@
+/*
+Diana Yonipha Zibi  
+219013187
+*/
+
+import javax.swing.*;
+public class Diana {
+  public static void main(String[] args) {
+  JOptionPane.showMessageDialog(null, "Programming is fun",null, JOptionPane.INFORMATION_MESSAGE);
+   System.exit(0);
+  } 
+}
